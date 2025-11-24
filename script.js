9c3a97d3d6803bfbdcdc2e7df35c0de8c6ed9a9b
kba <aguirrekenneth@gmail.com>
2025-11-24 19:30:19 +0800

    feat: capture email input on Enter keypress

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("KIDLAT WORKS script.js loaded!");
+
+const emailInput = document.getElementById("emailInput");
+
+function enterKeyDown(event) {
+  if (event.key == "Enter") {
+    const email = emailInput.value;
+    console.log("email value upon enter: ", email);
+    console.log("clearing email form...");
+    emailInput.value = "";
+  }
+}
+
+emailInput.addEventListener("keydown", enterKeyDown);
+
+// emailInput.addEventListener("keyup", (event) => {
+//   console.log("pressed key: ", event.target.value);
+// });
